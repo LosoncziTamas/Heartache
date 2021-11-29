eef81f4991bdb66323e49bdf9bfd77050761ce33
Losonczi Tamás <losonczi.tamas91@gmail.com>
2021-11-29 17:09:13 +0100

    player movement code WIP

--- Assets/Code/Hero/HeroController.cs
+++ Assets/Code/Hero/HeroController.cs
@@ -7,20 +7,34 @@ namespace Code.Hero
     {
         [SerializeField] private HeroProperties _heroProperties;
 
-        private float _localZ;
-
+        private Rigidbody2D _rigidbody2D;
+        
         private void Start()
         {
-            _localZ = transform.localPosition.z;
+            _rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
         private void Update()
         {
             var horizontal = Input.GetAxis("Horizontal");
             var vertical = Input.GetAxis("Vertical");
+
+            if (Mathf.Abs(horizontal) > 0 || Mathf.Abs(vertical) > 0)
+            {
+                var delta = new Vector2(horizontal, vertical);
+                var deltaNormalized = delta.normalized * _heroProperties.Speed * Time.deltaTime;
+                _rigidbody2D.velocity = deltaNormalized;
+            }
+            else
+            {
+                _rigidbody2D.velocity = Vector2.zero;
+            }
+
+            /*
             var delta = new Vector3(horizontal, vertical, _localZ).normalized;
             delta *= _heroProperties.Speed * Time.deltaTime;
-            transform.localPosition += delta;
+            transform.localPosition += delta;*/
+
         }
     }
 }
