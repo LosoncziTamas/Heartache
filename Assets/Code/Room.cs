69298fc67d90b9beac02eeacdf056814a837c875
Losonczi Tamás <losonczi.tamas91@gmail.com>
2021-12-01 21:26:56 +0100

    implement room-focusing camera

--- Assets/Code/Room.cs
+++ Assets/Code/Room.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -65,6 +66,78 @@ namespace Code
         private void Awake()
         {
             RoomSpawners.AddRange(GetComponentsInChildren<RoomSpawner>());
+        }
+        
+        
+        // TODO: extract to separate component
+        private static Room FocusedRoom { get; set; }
+        private bool _cameraIsMoving;
+
+        private IEnumerator MoveCamera(Camera cameraToMove, Vector3 target, float duration)
+        {
+            var accumulated = 0f;
+            var startPos = cameraToMove.transform.position;
+            _cameraIsMoving = true;
+            while (accumulated < duration && _cameraIsMoving)
+            {
+                var t = Mathf.Clamp01(accumulated / duration);
+                cameraToMove.transform.position = Vector3.Lerp(startPos, target, t);
+                accumulated += Time.deltaTime;
+                yield return null;
+            }
+            _cameraIsMoving = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(Tags.Player))
+            {
+                if (FocusedRoom != this)
+                {
+                    if (FocusedRoom != null)
+                    {
+                        FocusedRoom.RevokeFocus();
+                    }
+                    FocusedRoom = this;
+                    var cameraToMove = Camera.main;
+                    if (cameraToMove != null)
+                    {
+                        var pos = transform.position;
+                        var target = new Vector3(pos.x, pos.y, cameraToMove.transform.position.z);
+                        StartCoroutine(MoveCamera(cameraToMove, target, 1.0f));
+                    }
+                }
+            }
+        }
+        
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(Tags.Player))
+            {
+                if (FocusedRoom != this)
+                {
+                    var heroPos = other.transform.position;
+                    var currentRoomDiff = Vector3.Distance(FocusedRoom.transform.position, heroPos);
+                    var myRoomDiff = Vector3.Distance(transform.position, heroPos);
+                    if (myRoomDiff < currentRoomDiff)
+                    {
+                        FocusedRoom.RevokeFocus();
+                        FocusedRoom = this;
+                        var cameraToMove = Camera.main;
+                        if (cameraToMove != null)
+                        {
+                            var pos = transform.position;
+                            var target = new Vector3(pos.x, pos.y, cameraToMove.transform.position.z);
+                            StartCoroutine(MoveCamera(cameraToMove, target, 1.0f));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void RevokeFocus()
+        {
+            _cameraIsMoving = false;
         }
     }
 }
